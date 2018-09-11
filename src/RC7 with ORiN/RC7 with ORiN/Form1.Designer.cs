303fb5d5dfae44b1962c58d77f4a5fd6bf8ca194
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-09-11 16:35:05 -0700

    Adding sample files

---
+++ src/RC7 with ORiN/RC7 with ORiN/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RC7_with_ORiN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_ReadVariable = new System.Windows.Forms.Button();
+            this.button_WriteVariable = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_VariableContent = new System.Windows.Forms.Label();
+            this.textBox_WriteVar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(13, 13);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 0;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_ReadVariable
+            // 
+            this.button_ReadVariable.Location = new System.Drawing.Point(13, 42);
+            this.button_ReadVariable.Name = "button_ReadVariable";
+            this.button_ReadVariable.Size = new System.Drawing.Size(131, 23);
+            this.button_ReadVariable.TabIndex = 1;
+            this.button_ReadVariable.Text = "Read Variable S10";
+            this.button_ReadVariable.UseVisualStyleBackColor = true;
+            this.button_ReadVariable.Click += new System.EventHandler(this.button_ReadVariable_Click);
+            // 
+            // button_WriteVariable
+            // 
+            this.button_WriteVariable.Location = new System.Drawing.Point(13, 71);
+            this.button_WriteVariable.Name = "button_WriteVariable";
+            this.button_WriteVariable.Size = new System.Drawing.Size(131, 23);
+            this.button_WriteVariable.TabIndex = 2;
+            this.button_WriteVariable.Text = "Write Variable S10";
+            this.button_WriteVariable.UseVisualStyleBackColor = true;
+            this.button_WriteVariable.Click += new System.EventHandler(this.button_WriteVariable_Click);
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(13, 100);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(75, 23);
+            this.button_Close.TabIndex = 3;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Content of Variable S10";
+            // 
+            // label_VariableContent
+            // 
+            this.label_VariableContent.AutoSize = true;
+            this.label_VariableContent.Location = new System.Drawing.Point(167, 47);
+            this.label_VariableContent.Name = "label_VariableContent";
+            this.label_VariableContent.Size = new System.Drawing.Size(53, 13);
+            this.label_VariableContent.TabIndex = 5;
+            this.label_VariableContent.Text = "Unknown";
+            // 
+            // textBox_WriteVar
+            // 
+            this.textBox_WriteVar.Location = new System.Drawing.Point(170, 71);
+            this.textBox_WriteVar.MaxLength = 32;
+            this.textBox_WriteVar.Name = "textBox_WriteVar";
+            this.textBox_WriteVar.Size = new System.Drawing.Size(100, 20);
+            this.textBox_WriteVar.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 136);
+            this.Controls.Add(this.textBox_WriteVar);
+            this.Controls.Add(this.label_VariableContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_WriteVariable);
+            this.Controls.Add(this.button_ReadVariable);
+            this.Controls.Add(this.button_Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_ReadVariable;
+        private System.Windows.Forms.Button button_WriteVariable;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_VariableContent;
+        private System.Windows.Forms.TextBox textBox_WriteVar;
+    }
+}
+
---
+++ src/RC7 with ORiN/RC7 with ORiN/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;		// For Process handling
+using ORiN2.interop.CAO;        // Provide a path for the code below to the CAORCW dll that is in the References are of the projects.
+
+namespace RC7_with_ORiN
+{
+    public partial class Form1 : Form
+    {
+        // Global Variables
+
+        private CaoEngine ORiN_Engine;
+        private CaoController RC7Controller;
+        private CaoVariable RC7variable_S10;
+        private CaoTask RC7Task_RobSlave;
+
+        private int incrementValue = 0;
+
+        public Form1()
+        {
+            //This constructor for the graphical interface is called automatically upon program startup before the form is visible to the user
+            InitializeComponent();
+
+            //Disable the buttons to prevent exceptions from the user clicking them out of order
+            button_ReadVariable.Enabled = false;
+            button_WriteVariable.Enabled = false;
+            button_Close.Enabled = false;
+
+            KillCaoExecutableIfItIsAlreadyRunning();	// This is required to prevent lockup in the event that this program is not properly shut down
+
+            ORiN_Engine = new CaoEngine();				// This starts a separate program called "CAO.exe" which allows this program to talk with RC7 controllers
+        }
+
+        private static void KillCaoExecutableIfItIsAlreadyRunning()
+        {
+            // Kill the CAO.exe process in case it wasn't properly shut down last time
+            // For example, in case the user ran this code and then aborted it instead of shutting it down normally using the Close/Disconnect method below
+            Process[] caoProcesses = Process.GetProcessesByName("CAO");	// Retrieve all processes that have the name "CAO"
+
+            if ((caoProcesses != null) && (caoProcesses.Length > 0))	// If there is at least one "CAO" process...
+                caoProcesses[0].Kill();	// Kill the first process
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            button_Connect.Enabled = false;
+
+            // Create a link to the RC7 controller via the engine (CAO.exe)
+            
+            RC7Controller = ORiN_Engine.Workspaces.Item(0).AddController(
+                                "Sample",						// This is the name of the connection to the RC7 controller; Pass an empty string to have the controller auto-generate a name
+                                "CaoProv.DENSO.NetwoRC",	// This is the name of the DENSO ORiN provider for RC7; It must be exactly as shown
+                                "",             			// This is the name of the machine executing the DENSO ORiN provider (usually the machine running this program)
+                                "Conn=eth:192.168.0.2");	// This is the IP address of the physical RC7 controller
+
+            //In order to perform motion commands on the pc side you should start the RobSlave.pac program on the RC7
+            RC7Task_RobSlave = RC7Controller.AddTask("RobSlave", "");
+            RC7Task_RobSlave.Start(1, "");
+            
+
+            // Add a local variable that is linked to string variable S10 on the physical RC7 controller
+            RC7variable_S10 = RC7Controller.AddVariable("S10");
+
+            button_ReadVariable.Enabled = true;
+            button_WriteVariable.Enabled = true;
+            button_Close.Enabled = true;
+        }
+
+        private void button_ReadVariable_Click(object sender, EventArgs e)
+        {
+            ReadVariable();
+        }
+
+        private void ReadVariable()
+        {
+            // Reading from the local variable reads from the remote variable on the RC7 controller
+            label_VariableContent.Text = RC7variable_S10.Value.ToString();
+        }
+
+        private void button_WriteVariable_Click(object sender, EventArgs e)
+        {
+            // Write value of text box to S10 variable.
+            RC7variable_S10.Value = textBox_WriteVar.Text.ToString();
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            // Properly shut down the local variables in reverse order of precedence so that the ORiN engine is not left hanging
+            RC7Controller.Variables.Clear();	// Clear all local variables from the controller variable (THIS DOES NOT AFFECT VALUES ON THE PHYSICAL RC7 CONTROLLER)
+            RC7variable_S10 = null;				// Set the individual local variables to null
+
+            RC7Task_RobSlave.Stop(1,"");        //Stop the RobSlave program. (this is just a suggestion)
+            RC7Controller.Tasks.Clear();        // Clear all local tasks handles from the controller variable
+            RC7Task_RobSlave = null;            // Set the individual local variables to null
+
+            ORiN_Engine.Workspaces.Item(0).Controllers.Remove(RC7Controller.Index);	// Remove the controller variable from the ORiN engine
+            RC7Controller = null;				// Set the individual controller variable to null
+
+            ORiN_Engine = null;					// Set the ORiN engine variable to null
+
+            Close();							// Exit this C# program
+        }
+
+    }
+}
